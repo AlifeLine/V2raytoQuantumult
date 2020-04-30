6ba5f3bba5304fecd5a64400fc6b23014fa79153
AlifeLines <33149119+AlifeLines@users.noreply.github.com>
2020-04-30 12:24:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -63,18 +63,30 @@ def converter(url):
         strResult+="vmess://"+base64.b64encode(quanVmess.encode()).decode()+'\r'
     return base64.b64encode(strResult.encode()).decode()
 
-# @app.route('/sub')
-# def hello_world():
-#     print(request.full_path)
-#     full_path=request.full_path
-#     url=full_path[full_path.index("url"):].replace("url=","")
-#     other_payload=full_path[:full_path.index("url")]
-#     encode_url=urllib.parse.quote(url)
-#     full_request_payload="http://127.0.0.1:25500"+other_payload+"url="+encode_url
-#     sub_converet=requests.get(full_request_payload).text
-#     resp = make_response(sub_converet)
-#     resp.headers['content-type']= 'text/plain;charset=utf-8'
-#     return resp
+@app.route('/sub')
+def hello_world():
+    print(request.full_path)
+    full_path=request.full_path
+    url=full_path[full_path.index("url"):].replace("url=","")
+    other_payload=full_path[:full_path.index("url")]
+    encode_url=urllib.parse.quote(url)
+    full_request_payload="http://127.0.0.1:25500"+other_payload+"url="+encode_url
+    sub_converet=requests.get(full_request_payload).text
+    sub_converet=sub_converet.replace("http://127.0.0.1:25500","https://sub.v2speed.me/")
+    resp = make_response(sub_converet)
+    resp.headers['content-type']= 'text/plain;charset=utf-8'
+    return resp
+
+@app.route('/getruleset')
+def getruleset():
+    print(request.full_path)
+    full_path=request.full_path
+    full_request_payload="http://127.0.0.1:25500"+full_path
+    print(full_request_payload)
+    sub_converet=requests.get(full_request_payload).text
+    resp = make_response(sub_converet)
+    resp.headers['content-type']= 'text/plain;charset=utf-8'
+    return resp
 
 @app.route('/v2ray2quan')
 def v2ray2quan():
